5652826806f52d83bc891e87e3a2b5ebfda98406
Kauan <kauankenney16752@gmail.com>
2024-05-01 09:42:32 -0300

    Primeiro commit

---
+++ src/main/java/com/projetolabquimica/projetoLab/ProjetoLabApplication.java
@@ -0,0 +1,13 @@
+package com.projetolabquimica.projetoLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetolabquimica/projetoLab/ProjetoLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetolabquimica.projetoLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
